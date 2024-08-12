8f088f39049946985658ba8b0586f4cc7ced7ea0
Neo Joel <76043418+Champagne27@users.noreply.github.com>
2024-08-12 16:37:58 +0200

    Add files via upload

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_2_NWU_Tech_Trends
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+
+
+    }
+}
